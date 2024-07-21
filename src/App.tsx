f19f980d19e552ab3b1615b8b2955853be79e0d7
hossamg122 <hossamg122@gmail.com>
2024-07-21 20:01:14 +0300

    refactor: clear useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { increment, reset } from './features/counter/counterSlice'
-import { RootState } from './store'
-import { useTranslation } from 'react-i18next'
 import './i18n/config'
+import './App.css'
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value)
-  const dispatch = useDispatch()
-  const { t } = useTranslation()
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => dispatch(increment())}>
-          {t('count')} {count}
-        </button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div>welcome to github search</div>
     </>
   )
 }
